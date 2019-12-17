baccb942bcd3fdaa05e5cb64c57cab6ba42ced1a
yuweijun <yuweijun@live.com>
2019-12-17 21:45:26 +0800

    spring 2.5.6 spring-ant example

--- spring-ant/src/com/example/spring/controller/HomeController.java
+++ spring-ant/src/com/example/spring/controller/HomeController.java
@@ -1,10 +1,13 @@
 package com.example.spring.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
 
 /**
  * @author yuweijun
@@ -12,11 +15,12 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class HomeController extends AbstractController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
 
     @Override
     protected ModelAndView handleRequestInternal(HttpServletRequest request,
                                                  HttpServletResponse response) throws Exception {
-
+        LOGGER.info("date : {}", new Date());
         ModelAndView model = new ModelAndView("index");
         model.addObject("message", "hello world");
 
